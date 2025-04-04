b6c5d5e144571ce9e78be77737a35129d15783f7
wael2013 <wael.radwan@gmail.com>
2025-04-04 23:08:50 +0200

    Add CMake build system configuration

---
+++ build/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/calculator.cpp.obj"
+  "CMakeFiles/calculator.dir/calculator.cpp.obj.d"
+  "libcalculator.a"
+  "libcalculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/calculator_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator_test.dir/test.cpp.obj"
+  "CMakeFiles/calculator_test.dir/test.cpp.obj.d"
+  "calculator_test.exe"
+  "calculator_test.exe.manifest"
+  "calculator_test.pdb"
+  "libcalculator_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
